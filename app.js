7533eaa4d56aae4d54d350b94e771befbd5afbe5
Swift <theycallmeswift@gmail.com>
2012-10-06 12:52:26 -0400

    Added Database.

--- app.js
+++ app.js
@@ -1,27 +1,71 @@
-var Server = require('./lib/server')
+var util = require('util')
   , express = require('express')
   , app = express.createServer()
-  , rfidServer = new Server();
+  , RFIDServer = require('./lib/server')
+  , DB = require('./lib/db')
+  , TimeStamp = require('mongodb').Timestamp
+  , config = require('./config.json')
+  , rfidserver = new RFIDServer();
 
-
-app.listen(8080);
-
-app.use(express.static(__dirname + "/public"));
-
-app.get('/', function (req, res) {
-    res.sendfile(__dirname + '/public/index.html');
+app.configure(function() {
+  app.use(express.static(__dirname + "/public"));
 });
 
 app.get('/feed', function (req, res) {
-    res.sendfile(__dirname + '/public/feed.html');
+  res.sendfile(__dirname + '/public/feed.html');
 });
 
-rfidServer.on('listening', function() {
-  console.log('Listening');
+rfidserver.on('data', function(rfid) {
+
+  DB.collection('rfids', function(err, rfids) {
+    var query
+      , now = new Date()
+      , ten = new Date(now)
+      , timestamp = now.getTime();
+
+    ten.setSeconds(now.getSeconds() - 5);
+
+    console.log(timestamp);
+
+    if(err) {
+      return util.error(err);
+    }
+
+    query = {
+      rfid: rfid,
+      lastUpdate: { $lte: ten.getTime() }
+    };
+
+    rfids.findAndModify(query, [['_id','asc']], {$set: {lastUpdate: timestamp }}, { new: true }, function(err, object) {
+      console.log(err, object);
+      if(object) {
+        var inFridge = !object.inFridge;
+        rfids.update({ _id: object._id }, { $set: { inFridge: inFridge }}, {safe:true}, function(err) {
+          if(err) {
+            return util.error(err);
+          }
+          util.log(rfid + " ~ " + inFridge);
+        });
+      }
+    });
+  });
 });
 
-rfidServer.on('data', function(data) {
-  console.log("Data: ", data);
-});
+DB.connect(config.mongo);
 
-rfidServer.listen(1337);
+DB.on('connected', function() {
+  util.log("Connected to '" + config.mongo.database + "' database");
+  DB.collection('rfids', function(err, rfids) {
+    rfids.update({ }, { $set: { lastUpdate: new Date().getTime() }, $unset: { inFridge: 1 }}, { safe: true }, function(err) {
+      if(err) throw err;
+
+      rfidserver.listen(1337, function() {
+        util.log("TCP Server listening on port 1337");
+      });
+
+      app.listen(3000, function() {
+        util.log("HTTP Server listening on port 3000");
+      });
+    });
+  });
+});
