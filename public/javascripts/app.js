559ec9fcc17f4c935c81006b5f293b44563b2424
Jonathan Gottfried <jonmarkgo@gmail.com>
2012-10-06 16:23:54 -0400

    added socket listeners and more icons

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -73,5 +73,59 @@
       }]
     });
   });
+  $doc.ready(function() {
+    var shelf = $("#emptyshelf");
+    var socket = io.connect('/');
+    socket.on('insert', function (id, type) {
+      image = "<img id='"+id+"' src='images/"+type+".svg' />";
+      image.appendTo(shelf).fadeIn("slow");
+    });
+    socket.on('remove', function (id, type) {
+      image = $("#"+id);
+      image.fadeOut("slow");
+      var musicHTML = '';
+      var music = $("#music");
+      if (type == 'champagne') {
+        musicHTML = '<embed autoplay="true" height="0" width="0" src="http://jonsplanet.com/getiton.mp3" />';
+      }
+      else if (type == 'chocolate') {
+        musicHTML = '<iframe width="0" height="0" src="http://www.youtube.com/embed/k9B_6PH4dhU?autoplay=1&start=22" frameborder="0" allowfullscreen></iframe>';
+      }
+      else if (type == 'soda') {
+        musicHTML = '<iframe width="0" height="0" src="http://www.youtube.com/embed/Q8H5263jCGg?autoplay=1&start=1" frameborder="0" allowfullscreen></iframe>';
+      }
+      else if (type == 'pretzel') {
+        musicHTML = '<iframe width="0" height="0" src="http://www.youtube.com/embed/pVlr4g5-r18?autoplay=1&start=13" frameborder="0" allowfullscreen></iframe>';
+      }
+      music.html(musicHTML);
+      setTimeout(function(){ 
+        music.html('');
+      }, 10000 );
+    });
+    socket.on('nutrition', function (calories,fat,carbs,protein,sodium) {
+      chart.series[0].setData([{y: calories, color: 'black'},{y:fat, color: 'green'},{y:carbs,color:'red'},{y:protein,color:'yellow'},{y:sodium,color:'blue'}]);
 
+    });
+    socket.on('expired', function(title, type) {
+      var alert = $("#alert");
+      alert.html('<div class="four columns"><img src="images/'+type+'.svg" /></div><div class="eight columns"><p>Oh snap, your '+title+' is expired!</p></div>');
+      alert.fadeIn('slow');
+      setTimeout(function(){ 
+        alert.fadeOut("slow"); 
+      }, 5000 );
+    });
+    socket.on('protected', function(title, type) {
+      var alert = $("#alert");
+      var music = $("#music");
+      alert.html('<div class="four columns"><img src="images/'+type+'.svg" /></div><div class="eight columns"><p>Someone is trying to jack your '+title+'!</p></div>');
+      alert.fadeIn('slow');
+      setTimeout(function(){ 
+        alert.fadeOut("slow"); 
+      }, 5000 );
+      music.html('<iframe width="0" height="0" src="http://www.youtube.com/embed/KgmO32IdwuE?autoplay=1&start=1" frameborder="0" allowfullscreen></iframe>');
+      setTimeout(function(){ 
+        music.html('');
+      }, 10000 );
+    });
+  });
 })(jQuery, this);
